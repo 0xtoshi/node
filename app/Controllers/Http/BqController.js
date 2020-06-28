9dc88c0b155f564bc3ec0758bf5266617e48d2c9
Gilang Dandung <42947375+ximplah@users.noreply.github.com>
2020-06-29 03:28:45 +0700

    UPDATE BQ DONE

--- app/Controllers/Http/BqController.js
+++ app/Controllers/Http/BqController.js
@@ -275,48 +275,81 @@ class BqController {
             lama_tender : post.lama_tender
         }
 
-        await BQ.updateOrCreate(param_tender, { id : post.id})
-
+        await BQ.updateOrCreate({ id : post.id}, param_tender)
+        
+        var id_personil = post.id_personil
         var personil_jabatan = post.personil_jabatan
         var personil_gaji  = post.personil_gaji
         var personil_jumlah = post.personil_jumlah
 
         for (let i = 0; i < personil_jabatan.length; i++) {
 
+            if( id_personil[i] == 'new' ) { 
+                await Personil.create({
+                    id_bq : post.id,
+                    jabatan : personil_jabatan[i],
+                    gaji : personil_gaji[i],
+                    jumlah : personil_jumlah[i]
+                })
+
+            }else{
+
             await Personil.updateOrCreate({
-                id_bq : post.id,
-                jabatan : personil_jabatan[i],
-                gaji : personil_gaji[i],
-                jumlah : personil_jumlah[i],
-            }, { id_bq : post.id,
+                id : id_personil[i],
+            }, {
                 jabatan : personil_jabatan[i],
                 gaji : personil_gaji[i],
                 jumlah : personil_jumlah[i] })
-            
+
+            }
         }
         
+        var id_perlengkapan = post.id_perlengkapan
         var nama_perlengkapan = post.nama_perlengkapan
         var nominal_perlengkapan = post.nominal_perlengkapan
         var jumlah_perlengkapan = post.jumlah_perlengkapan
         for (let p = 0; p < nama_perlengkapan.length; p++) {
 
+            if( id_perlengkapan[p] == 'new' ) { 
+                await Perlengkapan.create({
+                    id_bq : post.id,
+                    nama : nama_perlengkapan[p],
+                    nominal : nominal_perlengkapan[p],
+                    jumlah : jumlah_perlengkapan[p]
+                })
+
+            }else{
             await Perlengkapan.updateOrCreate({
+                id : id_perlengkapan[p]
+            }, { 
                 nama : nama_perlengkapan[p],
                 nominal : nominal_perlengkapan[p],
-                jumlah : jumlah_perlengkapan[p],
-            }, { id_bq : post.id })
+                jumlah : jumlah_perlengkapan[p], }) 
+            }
         }
 
+        var id_lain2 = post.id_lain2
         var nama_lain2 = post.nama_lain2
         var nominal_lain2 = post.nominal_lain2
         var jumlah_lain2 = post.jumlah_lain2
         for (let l = 0; l < nama_lain2.length; l++) {
-            
+            if( id_lain2[l] == 'new' ) { 
+                await Lain2.create({
+                    id_bq : post.id,
+                    nama : nama_lain2[l],
+                    nominal : nominal_lain2[l],
+                    jumlah : jumlah_lain2[l]
+                })
+
+            }else{
             await Lain2.updateOrCreate({
+                id : id_lain2[l]
+            }, {
                 nama : nama_lain2[l],
                 nominal : nominal_lain2[l],
                 jumlah : jumlah_lain2[l],
-            }, { nama : nama_lain2[l] , id_bq : post.id })
+            })
+            }
         }
 
         

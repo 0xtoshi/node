b859797f58a2a3639c7e62e6c878d799861c6485
Gilang Dandung <42947375+ximplah@users.noreply.github.com>
2020-06-29 14:43:16 +0700

    Update Migration
    Update Migration Change CaseSensitive in Linux

--- database/migrations/1592879210416_tender_schema.js
+++ database/migrations/1592879210416_tender_schema.js
@@ -10,6 +10,8 @@ class TenderSchema extends Schema {
       table.integer('bulan', 2)
       table.integer('nominal', 11)
       table.integer('id_bq').unsigned().references('id').inTable('bqs').onDelete('cascade')
+      table.enum('category',['personil','perlengkapan','lain2'])
+      table.integer('ids',11)
       table.timestamps()
     })
   }
